5c970603107a72402f502d614881662ced2170bf
Shahed <Skrony0380@gmail.com>
2023-09-08 22:45:06 -0700

    fist commit

---
+++ Stewie.java
@@ -0,0 +1,9 @@
+public class Stewie {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////");
+        for (int i = 1; i <= 7; i++) {
+            System.out.println("|| Hello USA! ||");
+        }
+        System.out.println("//////////////////////");
+    }
+}
